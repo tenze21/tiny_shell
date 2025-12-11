a1449624f76616e97fdc56515b8e0b5146fe1a6a
tenze21 <tenzinchoda365@gmail.com>
2025-12-11 21:00:10 +0600

    --allow-empty

--- app/main.c
+++ app/main.c
@@ -12,7 +12,7 @@ int main() {
   char input[100];
   fgets(input, sizeof(input), stdin);
 
-  //fgets also saves the newline character added when user presses enter, so we want to remove that newline character.
+  //fgets also saves the newline character added, when user presses enter, so we want to remove that newline character.
   //strcspn searches for the first occurance in a string of any of the specified characters and returns the length of the string up to that point.
   input[strcspn(input, "\n")]= '\0';
   printf("%s: command not found\n", input);
