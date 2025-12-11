c3adad1574de71d26df0ee217c0c89e35bb10b02
tenze21 <tenzinchoda365@gmail.com>
2025-12-11 20:49:17 +0600

    --allow-empty
    get user command

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "string.h"
 
 int main() {
   // Flush after every printf
@@ -9,6 +10,11 @@ int main() {
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
+  fgets(input, sizeof(input), stdin);
+
+  //fgets also saves the newline character added when user presses enter, so we want to remove that newline character.
+  //strcspn searches for the first occurance in a string of any of the specified characters and returns the length of the string up to that point.
+  input[strcspn(input, "\n")]= '\0';
+  printf("%s: command not found\n", input);
   return 0;
-}
+} 
