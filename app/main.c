ea55916a82164636ac8bad230908f2cb48aacd80
tenze21 <tenzinchoda365@gmail.com>
2025-12-11 21:07:39 +0600

    --allow-empty

--- app/main.c
+++ app/main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include "string.h"
+#include "stdbool.h"
+
+#define MAXLEN 100
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
-  fgets(input, sizeof(input), stdin);
-
-  //fgets also saves the newline character added, when user presses enter, so we want to remove that newline character.
-  //strcspn searches for the first occurance in a string of any of the specified characters and returns the length of the string up to that point.
-  input[strcspn(input, "\n")]= '\0';
-  printf("%s: command not found\n", input);
+  
+  char input[MAXLEN];
+  while(true){
+    printf("$ ");
+  
+    fgets(input, sizeof(input), stdin);
+  
+    //fgets also saves the newline character added, when user presses enter, so we want to remove that newline character.
+    //strcspn searches for the first occurance in a string of any of the specified characters and returns the length of the string up to that point.
+    input[strcspn(input, "\n")]= '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 } 
