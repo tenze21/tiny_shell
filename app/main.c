39e6c3fbcaffc923caf7bdf0cf862ebef31b43ed
tenze21 <tenzinchoda365@gmail.com>
2026-02-11 11:05:23 +0600

    extend type to search for executable files using PATH

--- app/main.c
+++ app/main.c
@@ -2,11 +2,21 @@
 #include "string.h"
 #include "stdbool.h"
 #include "stdlib.h"
+#include "unistd.h"
 
 #define ARRAYLEN(A) (sizeof(A)/sizeof(A[0]))
-#define MAXLEN 100
+#define MAXINPUTLEN 100
 #define ECHOLEN 4
 #define TYPELEN 4
+#define MAXPATHLEN 100
+
+#ifdef _WIN32
+  #define PATH_SEPERATOR ";"
+  #define SLASH "\\"
+#else
+  #define PATH_SEPERATOR ":"
+  #define SLASH "/"
+#endif
 
 
 static char *builtin_cmds[]={"echo", "type", "exit"};
@@ -19,11 +29,62 @@ static bool check_cmd(char *input, char *cmd){
   return true;
 }
 
-static bool is_builtin(char *cmd){
-  for(unsigned int i=0; i<ARRAYLEN(builtin_cmds); i++){
-        if(strcmp(builtin_cmds[i], &cmd[TYPELEN + 1])==0) return true;
-  }
-  return false;
+static char *find_in_path(char *cmd){
+  char *path_env=getenv("PATH");
+  if(!path_env) return NULL;
+  
+  #ifdef _WIN32
+  char *path_ext=getenv("PATHEXT");/*file extension for windows executables*/
+  if(!path_ext) path_ext=".COM;.EXE;.BAT;.CMD";
+  char *ext_cpy=strdup(path_ext);
+  #endif
+  
+  char *path_cpy=strdup(path_env);
+  char *dir= strtok(path_cpy, PATH_SEPERATOR);
+  #ifndef _WIN32
+    while(dir!=NULL){
+      char full_path[MAXPATHLEN];
+      snprintf(full_path, MAXPATHLEN, "%s/%s", dir, cmd);
+      if(access(full_path, F_OK | X_OK)==0){
+        free(path_cpy);
+        return strdup(full_path);
+      }
+      dir=strtok(NULL, PATH_SEPERATOR);
+    }
+  #else
+      while(dir!=NULL){
+        char full_path[MAXPATHLEN];
+        snprintf(full_path, MAXPATHLEN, "%s\\%s", dir, cmd);
+        // Try the command as-is first (might already have extension)
+        if(access(full_path, F_OK)){
+          free(path_cpy);
+          free(ext_cpy);
+          return strdup(full_path);
+        }
+
+        char *ext=strtok(ext_cpy, PATH_SEPERATOR);
+        while(ext!=NULL){
+          snprintf(full_path, MAXPATHLEN, "%s\\%s%s", dir, cmd, ext);
+          if(access(full_path, F_OK)==0){
+            free(path_cpy);
+            free(ext_cpy);
+            return strdup(full_path);
+          }
+          ext=strtok(NULL, PATH_SEPERATOR);
+        }
+        /*reset ext_cpy*/
+        free(ext_cpy);
+        ext_cpy=strdup(path_ext);
+
+        dir=strtok(NULL, PATH_SEPERATOR);
+      }
+  #endif
+  
+  free(path_cpy);
+  #ifdef _WIN32
+    free(ext_cpy);
+  #endif
+  return NULL;
 }
 
 int main() {
@@ -31,17 +92,14 @@ int main() {
   setbuf(stdout, NULL);
 
   
-  char input[MAXLEN];
+  char input[MAXINPUTLEN];
   while(true){
     printf("$ ");
   
     fgets(input, sizeof(input), stdin);
   
-    //fgets also saves the newline character added, when user presses enter, so we want to remove that newline character.
-    //strcspn searches for the first occurance in a string of any of the specified characters and returns the length of the string up to that point.
     input[strcspn(input, "\n")]= '\0';
 
-    //termination
     if(check_cmd(input, "exit")) break;
     else if(check_cmd(input, "echo")) 
       printf("%s\n", &input[ECHOLEN + 1]);//print only the string after echo and a space.
@@ -53,11 +111,21 @@ int main() {
           break;
         }
         i++;
-        if(i==ARRAYLEN(builtin_cmds)) printf("%s: not found\n", &input[TYPELEN + 1]);
+        if(i==ARRAYLEN(builtin_cmds)){
+          char *path_to_cmd= find_in_path(&input[TYPELEN + 1]);
+          if(path_to_cmd==NULL)
+            printf("%s: not found\n", &input[TYPELEN + 1]);
+          else{
+            printf("%s is %s\n", &input[TYPELEN + 1], path_to_cmd);
+            free(path_to_cmd);
+          }
+        }
+
       }
     }else{
       printf("%s: command not found\n", input);
     }
   }
+    
   return EXIT_SUCCESS;
 } 
