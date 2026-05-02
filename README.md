9a841229b93e66126773e75f10f8ebca383ae3c8
tenze21 <tenzinchoda365@gmail.com>
2026-05-02 20:36:00 +0600

    add autocomplete for builtin commands using readl

--- README.md
+++ README.md
@@ -1,34 +1,12 @@
-[![progress-banner](https://backend.codecrafters.io/progress/shell/9a391569-acfd-4062-b07d-534196db1c0c)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
+# TinyShell
+A simple shell program I am writing to learn C programming. I am building it by following the ["Build Your Own Shell" Challenge](https://app.codecrafters.io/courses/shell/overview) on [codecrafters.io](https://codecrafters.io).
 
-This is a starting point for C solutions to the
-["Build Your Own Shell" Challenge](https://app.codecrafters.io/courses/shell/overview).
+## Why am I learning C?
+To discuss this let's go back to before I learned C. I started programming 2 years ago from now and I was writing mostly `javascript` and `python`, but languages like these have most of the stuff abstracted from the programmer and I had this urge to take a peek beneath the abstraction, then I learned that `javascript` and `python` were also written in a programming language called `C`(Solved one of my high school mysteries of "if programming languages were used to write instructions for the computers then how are the programming languages themselves built?") and that's when I decided to start learning `C`. I am really not sure if learning C will be of any use to me in future but the thing that does keep me glued to this language, that has almost nothing compared to any modern language is that I learn a lot about the machine itself when I program in C.
 
-In this challenge, you'll build your own POSIX compliant shell that's capable of
-interpreting shell commands, running external programs and builtin commands like
-cd, pwd, echo and more. Along the way, you'll learn about shell command parsing,
-REPLs, builtin commands, and more.
-
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
-
-# Passing the first stage
-
-The entry point for your `shell` implementation is in `app/main.c`. Study and
-uncomment the relevant code, and push your changes to pass the first stage:
-
-```sh
-git commit -am "pass 1st stage" # any msg
-git push origin master
-```
-
-Time to move on to the next stage!
-
-# Stage 2 & beyond
-
-Note: This section is for stages 2 and beyond.
-
-1. Ensure you have `c (9.2)` installed locally
-1. Run `./your_program.sh` to run your program, which is implemented in
-   `app/main.c`.
-1. Commit your changes and run `git push origin master` to submit your solution
-   to CodeCrafters. Test output will be streamed to your terminal.
+## Features
+- 5 Builtin commands: `echo`, `type`, `exit`, `pwd`, `cd` all have the same functionaly as their `bash` counterpart.
+- Support for quotes, single/double
+- Support for escaping characters with backslash
+- Executing external programs like `git`.
+- Redirecting `stdout` and `stderr`.  
--- app/main.c
+++ app/main.c
@@ -6,10 +6,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #define ARRAYLEN(A) (sizeof(A)/sizeof(A[0]))
 #define MAXINPUTLEN 100
@@ -21,8 +24,8 @@
 #define MAXPATHLEN 1024
 #define PATH_SEPERATOR ":"
 
-static char *builtin_cmds[]={"echo", "type", "exit", "pwd", "cd"};
-static char *redirect_ops[]={">", ">>", "1>>", "1>", "2>", "2>>"};
+static const char *builtin_cmds[]={"echo", "type", "exit", "pwd", "cd", NULL};
+static const char *redirect_ops[]={">", ">>", "1>>", "1>", "2>", "2>>", NULL};
 
 typedef struct{
     char *argv[MAXARGS];
@@ -76,14 +79,16 @@ static void change_dir(const char *path){
     }
 }
 
-static int is_shell_builtin(char *cmd){
-    for(int i=0; i<ARRAYLEN(builtin_cmds); i++){
+static bool is_shell_builtin(char *cmd){
+    int idx=0;
+    const char *builtin;
+    while((builtin= builtin_cmds[idx++]) != NULL){
       // Check if command is a builtin shell command
-      if(strcmp(builtin_cmds[i], cmd)==0){
-          return i;
+      if(strcmp(builtin, cmd)==0){
+          return true;
       }
     }
-    return -1;
+    return false;
 }
 
 /*
@@ -100,7 +105,7 @@ static void trim(char *str){
 }
 
 /*
- * @dev token input string into seperate command arguments.
+ * @dev tokenize input string into seperate command arguments.
  *
  * writes each command token to argv.
  */
@@ -169,8 +174,10 @@ static int tokenize_cmd(const char *input, char *argv[]){
 }
 
 static bool is_redirect_op(char *token){
-    for(int i=0; i<ARRAYLEN(redirect_ops); i++){
-        if(strcmp(token, redirect_ops[i])==0) return true;
+    int idx=0;
+    const char *op;
+    while((op= redirect_ops[idx++]) != NULL){
+        if(strcmp(token, op)==0) return true;
     }
     return false;
 }
@@ -207,7 +214,7 @@ static int parse_cmd(const char *input, command_t *cmd){
 
             cmd->out_append=is_out_append;
             cmd->err_append=is_err_append;
-            
+
             if(is_redirect_out || is_out_append)
                 cmd->redirect_out=argv[i+1];
             if(is_redirect_err || is_err_append)
@@ -266,99 +273,140 @@ static void free_args(char *argv[], const size_t argc){
     }
 }
 
-int main(void) {
-  while(true){
-    // Flush after every printf
-    setbuf(stdout, NULL);
-    char input[MAXINPUTLEN]={'\0'};
-    command_t cmd={0};
-    printf("$ ");
+/*
+ * READLINE UTILITIES
+*/
 
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")]= '\0';
-    trim(input);
+/**
+ * @dev Readline calls this repeatedly until it returns NULL, when user presses TAB.
+ * @param text - The partial word the user has typed so far.
+ * @param state - 0 on first call, increments each subsequent call. used by readline to determine if it's a fresh call.
+ */
+static char *command_generator(const char *text, int state){
+    static int idx;
+    static int len;
+    const char *name;
     
-    if(parse_cmd(input, &cmd)<0) continue;
-
-    int saved_stdout=-1;
-    int saved_stderr=-1;
-    if(apply_redirects(&cmd, &saved_stdout, &saved_stderr)<0) continue;
-
-    if(strcmp(cmd.argv[0], "exit")==0)
-    {
-        break;
+    // state=0 implies it's a fresh completion call, reset the index
+    if(state==0){
+        idx=0;
+        len=strlen(text);
     }
-    else if(strcmp(cmd.argv[0], "echo")==0)
-    {
-        for(int i=1; i<cmd.argc; i++){
-            if(i>1) printf(" ");
-            printf("%s", cmd.argv[i]);
+    
+    // Walk through `builtin_cmds` and return the next match
+    while((name=builtin_cmds[idx++]) != NULL){
+        // Check if name starts wih whatever the user typed
+        if(strncmp(name, text, len) == 0)
+            return strdup(name);
+    }
+    
+    return NULL;
+}
+
+/*
+ * @dev readline calls this function first, this allows you to do context aware completion by passing different `command_generator` functions depending on the first word.
+ */
+static char **command_completion(const char *text, int start, int end){
+    // start = 0, means we are completing the first word, the command itself.
+    // start > 0, means we are completing an argument, this can be handles differently depending on the command that was passed.
+    if(start==0)
+        return rl_completion_matches(text, command_generator);
+    
+    return NULL;
+}
+
+int main(void) {
+    rl_readline_name= "tinyShell";
+    
+    // Wire up completion function
+    rl_attempted_completion_function= command_completion;
+    char *line;
+    while((line = readline("$ ")) != NULL){
+        
+        if(*line!='\0') add_history(line);
+        
+        command_t cmd={0};
+    
+        if(parse_cmd(line, &cmd)<0) continue;
+    
+        int saved_stdout=-1;
+        int saved_stderr=-1;
+        if(apply_redirects(&cmd, &saved_stdout, &saved_stderr)<0) continue;
+    
+        if(strcmp(cmd.argv[0], "exit")==0)
+        {
+            break;
         }
-        printf("\n");
-    }
-    else if(strcmp(cmd.argv[0], "type")==0)
-    {
-        int idx=is_shell_builtin(cmd.argv[1]);
-        if(idx>=0){
-          printf("%s is a shell builtin\n", builtin_cmds[idx]);
-        }else{
-            char *path_to_cmd= find_in_path(cmd.argv[1]);
-            if(path_to_cmd==NULL)
-                printf("%s: not found\n", cmd.argv[1]);
-            else{
-                printf("%s is %s\n", cmd.argv[1], path_to_cmd);
-                free(path_to_cmd);
+        else if(strcmp(cmd.argv[0], "echo")==0)
+        {
+            for(int i=1; i<cmd.argc; i++){
+                if(i>1) printf(" ");
+                printf("%s", cmd.argv[i]);
+            }
+            printf("\n");
+        }
+        else if(strcmp(cmd.argv[0], "type")==0)
+        {
+            if(is_shell_builtin(cmd.argv[1])){
+                printf("%s is a shell builtin\n", cmd.argv[1]);
+            }else{
+                char *path_to_cmd= find_in_path(cmd.argv[1]);
+                if(path_to_cmd==NULL)
+                    printf("%s: not found\n", cmd.argv[1]);
+                else{
+                    printf("%s is %s\n", cmd.argv[1], path_to_cmd);
+                    free(path_to_cmd);
+                }
             }
         }
-    }
-    else if(strcmp(cmd.argv[0], "pwd")==0)
-    {
-        char current_working_directory[MAXPATHLEN];
-        if(getcwd(current_working_directory, sizeof(current_working_directory))!=NULL){
-            printf("%s\n", current_working_directory);
-        }else{
-            perror("error: failed to get current working directory.\n");
-        }
-    }
-    else if(strcmp(cmd.argv[0], "cd")==0)
-    {
-        char new_dir[MAXPATHLEN];
-        snprintf(new_dir, MAXPATHLEN, "%s",cmd.argv[1]);
-        if(strcmp(new_dir, "~")==0){
-            char *path_to_home=getenv("HOME");
-            change_dir(path_to_home);
-        }else{
-            change_dir(new_dir);
-        }
-    }
-    else
-    {
-        char *path_to_cmd=find_in_path(cmd.argv[0]);
-
-        if(path_to_cmd != NULL)//execute the system command
+        else if(strcmp(cmd.argv[0], "pwd")==0)
         {
-            pid_t id=fork();
-            if(id==-1){
-                perror("error: fork failed");
-                return EXIT_FAILURE;
-            }else if(id==0){
-                execvp(cmd.argv[0], cmd.argv);
-                fprintf(stderr, "error: failed to execute %s\n", path_to_cmd);
-                fflush(stdout);
-                _exit(EXIT_FAILURE);
+            char current_working_directory[MAXPATHLEN];
+            if(getcwd(current_working_directory, sizeof(current_working_directory))!=NULL){
+                printf("%s\n", current_working_directory);
             }else{
-                wait(NULL);
+                perror("error: failed to get current working directory.\n");
+            }
+        }
+        else if(strcmp(cmd.argv[0], "cd")==0)
+        {
+            char new_dir[MAXPATHLEN];
+            snprintf(new_dir, MAXPATHLEN, "%s",cmd.argv[1]);
+            if(strcmp(new_dir, "~")==0){
+                char *path_to_home=getenv("HOME");
+                change_dir(path_to_home);
+            }else{
+                change_dir(new_dir);
             }
         }
         else
         {
-            fprintf(stderr, "%s: command not found\n", cmd.argv[0]);
+            char *path_to_cmd=find_in_path(cmd.argv[0]);
+    
+            if(path_to_cmd != NULL)//execute the system command
+            {
+                pid_t id=fork();
+                if(id==-1){
+                    perror("error: fork failed");
+                    return EXIT_FAILURE;
+                }else if(id==0){
+                    execv(path_to_cmd, cmd.argv);
+                    fprintf(stderr, "error: failed to execute %s\n", path_to_cmd);
+                    fflush(stdout);
+                    _exit(EXIT_FAILURE);
+                }else{
+                    wait(NULL);
+                }
+            }
+            else
+            {
+                fprintf(stderr, "%s: command not found\n", cmd.argv[0]);
+            }
+            free(path_to_cmd);
         }
-        free(path_to_cmd);
+        free_args(cmd.argv, cmd.argc);
+    
+        restore_redirects(saved_stdout, saved_stderr);
     }
-    free_args(cmd.argv, cmd.argc);
-
-    restore_redirects(saved_stdout, saved_stderr);
-  }
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
