5a3ff9cb706a167e621afec7e5cad06fb868ac8e
tenze21 <tenzinchoda365@gmail.com>
2026-05-03 19:37:55 +0600

    codecrafters submit [skip ci]

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+#
+# This script is used to compile your program on CodeCrafters
+# 
+# This runs before .codecrafters/run.sh
+#
+# Learn more: https://codecrafters.io/program-interface
+
+# Exit early if any commands fail
 set -e
 
-# Print environment info for debugging
-echo "VCPKG_ROOT is: ${VCPKG_ROOT}"
-echo "vcpkg location: $(which vcpkg 2>/dev/null || echo 'not in PATH')"
-ls /usr/local/vcpkg 2>/dev/null && echo "found at /usr/local/vcpkg" || echo "not at /usr/local/vcpkg"
-
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-cmake --build ./build
+gcc app/*.c -lreadline -o /tmp/shell-target
